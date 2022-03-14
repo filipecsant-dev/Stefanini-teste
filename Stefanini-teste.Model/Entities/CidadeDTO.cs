b1e9a0114422e89e5bb5153a9490c257ad805406
Filipe Castro <filipecsant@gmail.com>
2022-03-14 15:13:46 -0300

    Criando entidades

---
+++ Stefanini-teste.Model/Entities/CidadeDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stefanini.Model.Entities
+{
+    [Table("Cidade")]
+    public class CidadeDTO
+    {
+        [Key]
+        public int CodCidade { get; set; }
+        [MaxLength(200)]
+        [Required]
+        public string Nome { get; set; }
+        [MaxLength(20)]
+        [Required]
+        public string UF { get; set; }
+    }
+}
---
+++ Stefanini-teste.Model/Entities/PessoaDTO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stefanini.Model.Entities
+{
+    [Table("Pessoa")]
+    public class PessoaDTO : Disable
+    {
+        [Key]
+        public int CodPessoa { get; set; }
+
+        [MaxLength(300)]
+        [Required]
+        public string Nome { get; set; }
+
+        [MaxLength(11)]
+        [Required]
+        public string CPF { get; set; }
+
+        [Required]
+        public int CodCidade { get; set; }
+        public CidadeDTO Cidade { get; set; }
+
+        [Required]
+        public int Idade { get; set; }
+    }
+}
