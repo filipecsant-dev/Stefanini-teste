f6b272d596fc1b002d6124ec2d8cf939c5e2557e
Filipe Castro <filipecsant@gmail.com>
2022-03-14 15:44:24 -0300

    Configurando Entity e criado migration

---
+++ Stefanini-teste.DAO/Migrations/20220314184302_nova4.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Stefanini.DAO.Migrations
+{
+    public partial class nova4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cidade",
+                columns: table => new
+                {
+                    CodCidade = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    UF = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cidade", x => x.CodCidade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pessoa",
+                columns: table => new
+                {
+                    CodPessoa = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    CPF = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    CodCidade = table.Column<int>(type: "int", nullable: false),
+                    CidadeCodCidade = table.Column<int>(type: "int", nullable: false),
+                    Idade = table.Column<int>(type: "int", nullable: false),
+                    Id = table.Column<long>(type: "bigint", nullable: false),
+                    IsDisabled = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoa", x => x.CodPessoa);
+                    table.ForeignKey(
+                        name: "FK_Pessoa_Cidade_CidadeCodCidade",
+                        column: x => x.CidadeCodCidade,
+                        principalTable: "Cidade",
+                        principalColumn: "CodCidade",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pessoa_CidadeCodCidade",
+                table: "Pessoa",
+                column: "CidadeCodCidade");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pessoa");
+
+            migrationBuilder.DropTable(
+                name: "Cidade");
+        }
+    }
+}
